239557079cc22b4d4bf7f807743b36f343a6bd80
harrisonsanches <harrison.sanches@gmail.com>
2019-11-04 14:20:53 -0300

    correção uva 11581

--- UVA_Problems/Codes/uva_11581.py
+++ UVA_Problems/Codes/uva_11581.py
@@ -1,4 +1,5 @@
-def cria_matriz(matriz):
+def cria_matriz():
+    matriz = []
     for i in range(3):
         vet = []
         linha = input()
@@ -8,34 +9,40 @@ def cria_matriz(matriz):
         matriz.append(vet)
 
     return matriz
+
 def eh_nula(matriz):
-    cont = 0
     for i in range(len(matriz)):
         for j in range(len(matriz[i])):
-            if matriz[i][j] == 0:
-                cont += 1
+            if matriz[i][j] > 0:
+                return False
+    return True
 
-    if cont == len(matriz)*3:
-        return 1
-    else:
-        return -1
-
-def cria_matriz_zerada(matriz):
+def cria_matriz_zerada():
     linha_com_zeros = [0] *3
-    matriz = [linha_com_zeros] * 3
-    return matriz
+    matriz_nova = [linha_com_zeros] * 3
+    return matriz_nova
 
+def transforma_matriz(matriz):
+    matriz_nova = []
+    linha = []
+    linha.append((matriz[0][1] + matriz[1][0]) % 2)
+    linha.append((matriz[0][0] + matriz[0][2] + matriz[1][1]) % 2)
+    linha.append((matriz[0][1] + matriz[1][2]) % 2)
+    matriz_nova.append(linha)
 
-def transforma_matriz(matriz_nova,matriz):
-    matriz_nova[0][1] =
-    matriz_nova[0][2] =
-    matriz_nova[0][3] =
-    matriz_nova[1][0] =
-    matriz_nova[1][1] =
-    matriz_nova[1][2] =
-    matriz_nova[2][0] =
-    matriz_nova[2][1] =
-    matriz_nova[2][2] =
+    linha = []
+    linha.append((matriz[0][0] + matriz[2][0] + matriz[1][1]) % 2)
+    linha.append((matriz[0][1] + matriz[1][0] + matriz[1][2] + matriz[2][1]) % 2)
+    linha.append((matriz[0][2] + matriz[1][1] + matriz[2][2]) % 2)
+    matriz_nova.append(linha)
+
+    linha = []
+    linha.append((matriz[1][0] + matriz[2][1]) % 2)
+    linha.append((matriz[2][0] + matriz[1][1] + matriz[2][2]) % 2)
+    linha.append((matriz[2][1] + matriz[1][2]) % 2)
+    matriz_nova.append(linha)
+
+    return matriz_nova
 
 
 def main(args):
@@ -44,18 +51,19 @@ def main(args):
         input()
         i = 0
         while i < quantidade:
-            matriz = []
-            matriz = cria_matriz(matriz)
-            nova_matriz = cria_matriz_zerada(nova_matriz)
-            nao_nula = True
+            matriz= cria_matriz()
+            if eh_nula(matriz):
+                print("-1")
+            else:
+                contador = 0
+                while not eh_nula(matriz):
+                    matriz = transforma_matriz(matriz)
+                    contador += 1
+                print(contador - 1)
 
-
-            while nao_nula:
-            #     recursão até encontrar uma matriz nula, depois é soó mandar quantas recursões fez - q
 
             input()
             i += 1
-
 
 
     except EOFError:
